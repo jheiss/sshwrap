53612a6eae7abc0ac28484d7e9b4402708e0cea7
Jason Heiss <jheiss@aput.net>
2013-03-13 18:47:02 -0400

    Recognize more password prompts
    Specifically, recognize the sudo prompt used on most Linux
    distributions.  Expand caching logic to store one password for each
    unique prompt we see.  Add a config file (~/.sshwrap.yaml) where the
    user can configure the password recognition regex to match unusual or
    site-specific prompts.
    
    Closes GH#2

--- lib/sshwrap/main.rb
+++ lib/sshwrap/main.rb
@@ -1,24 +1,43 @@
+require 'etc'
+require 'yaml'
 require 'net/ssh'
 require 'highline/import'
 
 class SSHwrap::Main
   def initialize(options={})
+    home = Dir.respond_to?(:home) ? Dir.home : ENV['HOME'] || ENV['LOGDIR']
+    conffile = "#{home}/.sshwrap.yaml"
+    if File.exist?(conffile)
+      conf = YAML.load_file(conffile)
+    else
+      conf = {}
+    end
+    
     @mutex = Mutex.new
     @max_workers = options[:max_workers] || 1
     @abort_on_failure = options[:abort_on_failure]
     @user = options[:user] || Etc.getlogin
     @ssh_key = options[:ssh_key]
     @debug = options[:debug]
-    @password = nil
+    @passwords = {}
+    @ssh_prompt = "Password for #{@user}: "
+    
+    if conf['password_regexp']
+      @password_regexp = Regexp.new('(' + conf['password_regexp'] + ')')
+    elsif options[:password_regexp]
+      @password_regexp = options[:password_regexp]
+    else
+      @password_regexp = /(Password: |\[sudo\] password for .*: )/
+    end
   end
   
-  def get_password
+  def get_password(prompt)
     @mutex.synchronize do
-      if !@password
-        @password = ask("Password for #{@user}: ") { |q| q.echo = "x" }
+      if !@passwords[prompt]
+        @passwords[prompt] = ask(prompt) { |q| q.echo = "x" }
       end
     end
-    @password
+    @passwords[prompt]
   end
   
   def ssh_execute(cmd, target)
@@ -31,9 +50,9 @@ class SSHwrap::Main
       params[:keys] = [@ssh_key]
     end
     using_password = false
-    if @password
+    if @passwords[@ssh_prompt]
       using_password = true
-      params[:password] = @password
+      params[:password] = @passwords[@ssh_prompt]
     end
     
     begin
@@ -63,9 +82,13 @@ class SSHwrap::Main
             # if it's a password prompt, and interactively return data if so
             # (see request_pty above).
             channel.on_data do |ch_data, data|
-              # This is the standard sudo password prompt
-              if data =~ /Password:/
-                channel.send_data "#{get_password}\n"
+              # sudo on Mac OS X:
+              #   Password:
+              # sudo on Red Hat, Debian, Ubuntu:
+              #   [sudo] password for <user>:
+              if data =~ @password_regexp
+                prompt = $1
+                channel.send_data "#{get_password(prompt)}\n"
               else
                 stdout << data unless (data.nil? or data.empty?)
               end
@@ -85,7 +108,7 @@ class SSHwrap::Main
       end
     rescue Net::SSH::AuthenticationFailed
       if !using_password
-        get_password
+        get_password(@ssh_prompt)
         return ssh_execute(cmd, target)
       else
         stderr << "Authentication failed to #{target}"
